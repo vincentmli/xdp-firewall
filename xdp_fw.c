180661c6d35ba7c71de010a23492d09b6c2fb34d
Vincent Li <vincent.mc.li@gmail.com>
2023-03-22 18:08:33 -0700

    include vmlinux.h for easy build and developing

--- xdp_fw.c
+++ xdp_fw.c
@@ -1,33 +1,21 @@
-//go:build ignore
-
+#include "vmlinux.h"
 #include "bpf_endian.h"
-#include "common.h"
-
-char __license[] SEC("license") = "Dual MIT/GPL";
+#include "bpf_helpers.h"
 
 #define MAX_MAP_ENTRIES 16
 #define IPV4_PREFIX_LEN 32
 
-enum {
-	BPF_F_NO_PREALLOC = (1U << 0),
-};
-
-/* Key of an a BPF_MAP_TYPE_LPM_TRIE entry */
-struct bpf_lpm_trie_key {
-        __u32   prefixlen;      /* up to 32 for AF_INET, 128 for AF_INET6 */
-        __u8    data[0];        /* Arbitrary size */
-};
 
 struct src_ip4_trie_key {
 	struct bpf_lpm_trie_key lpm_key;
-	__u8 saddr[4];
+	u8 saddr[4];
 };
 
 struct {
 	__uint(type, BPF_MAP_TYPE_LPM_TRIE);
 	__uint(max_entries, MAX_MAP_ENTRIES);
 	__type(key, struct src_ip4_trie_key);
-	__type(value, __u32);
+	__type(value, u32);
 	__uint(pinning, LIBBPF_PIN_BY_NAME);
 	__uint(map_flags, BPF_F_NO_PREALLOC);
 } firewall_map SEC(".maps");
@@ -38,7 +26,7 @@ struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 16);
     groupkey* key;
-    __type(value, __u32);
+    __type(value, u32);
 } group_map SEC(".maps");
 
 SEC("xdp")
@@ -84,9 +72,9 @@ int firewall(struct xdp_md *ctx) {
 	key.saddr[3]   = (ip->saddr >> 24) & 0xff;
 
 	if (tcp->dest == bpf_htons(8080)) {
-		__u32 *drops = bpf_map_lookup_elem(&firewall_map, &key);
+		u32 *drops = bpf_map_lookup_elem(&firewall_map, &key);
 		if (drops) { // source IP in block list, drop it
-			__u32 *value = 0;
+			u32 *value = 0;
 			groupkey gkey = "firewall_map";
 			value = bpf_map_lookup_elem(&group_map, &gkey);
 			/*
@@ -106,3 +94,5 @@ int firewall(struct xdp_md *ctx) {
 	// Try changing this to XDP_DROP and see what happens!
 	return XDP_PASS;
 }
+
+char __license[] SEC("license") = "Dual MIT/GPL";
